c31e6ab1c9af6d613860a8f7a35afecb045de8c9
Ericka Joy Formanes <formaneserickajoy@gmail.com>
2024-05-16 06:01:09 +0800

    Tokenizer

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-//imports
-const express = require("express");
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 5000;
 
-//API middleware
-// app.use(express.json());
-// app.use(express.urlencoded());
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(join(__dirname, 'public', 'css')));
+app.use('/js', express.static(join(__dirname, 'public', 'js')));
+app.use('/files', express.static(join(__dirname, 'public', 'files')));
 
+// Serve HTML file
+app.get('', (req, res) => {
+    res.sendFile(join(__dirname, 'src', 'index.html'));
+});
 
-//static files
-app.use(express.static(`public`))
-app.use(`/css`, express.static(__dirname + `public/css`))
-app.use(`/js`, express.static(__dirname + `public/js`))
-app.use(`/files`, express.static(__dirname + `public/files`))
-
-//Call html file
-app.get(``, (req, res) => {
-    res.sendFile(__dirname + `/src/index.html`)
-})
-
-
-//listen on port 
+// Listen on port 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`)
-})
+    console.log(`Server started at http://localhost:${port}`);
+});
